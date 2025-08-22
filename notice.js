8dc958bde6de99543efbbdf9f249153dc7d74bd3
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-22 16:35:19 +0900

    +notice.js

---
+++ notice.js
@@ -0,0 +1,7 @@
+// Palmu 公式のお知らせ情報
+const PALMU_NOTICES = [
+	{date: '2025-08-26T15:20', text: 'メンテナンス 14:00-15:30 [FB条件3，表示タブ，PayPay]',
+     url: 'https://x.com/palmu_jp/status/1958740914500735062'},
+	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
+     url: 'https://x.com/palmu_jp/status/1948548755935969349'},
+];
