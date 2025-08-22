4a3cfdb2fd3e2eaedc6aca249e87ecbec653de3b
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-22 21:29:11 +0900

    up notice.jp: palhub 2025-08-24

--- notice.js
+++ notice.js
@@ -1,6 +1,8 @@
 // Palmu 公式のお知らせ情報
 const PALMU_NOTICES = [
-	{date: '2025-08-26T15:20', text: 'メンテナンス 14:00-15:30 [FB条件3，表示タブ，PayPay]',
+	{date: '2025-08-24T14:30', text: 'PalHub 14:00-14:30',
+	 url: 'https://x.com/palmu_jp/status/1957763773932130628'},
+	{date: '2025-08-26T15:20', text: 'メンテ 14:00-15:30 [FB条件3，表示タブ，PayPay]',
      url: 'https://x.com/palmu_jp/status/1958740914500735062'},
 	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
      url: 'https://x.com/palmu_jp/status/1948548755935969349'},
