538b5fd82accd4d7ea6c8075259e60dccd0a8c41
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-23 14:21:18 +0900

    notice: サマージャンボ応援セール

--- notice.js
+++ notice.js
@@ -4,6 +4,8 @@ const PALMU_NOTICES = [
 	 url: 'https://x.com/palmu_jp/status/1957763773932130628'},
 	{date: '2025-08-26T15:20', text: 'メンテ 14:00-15:30 [FB条件3，表示タブ，PayPay]',
      url: 'https://x.com/palmu_jp/status/1958740914500735062'},
+	{date: '2025-08-31', text: 'セール 8/25-8/31',
+	 url: 'https://x.com/palmu_jp/status/1959088203040002408'},
 	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
      url: 'https://x.com/palmu_jp/status/1948548755935969349'},
 ];
