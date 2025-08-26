7769458aaca287954af4fd1ec6fd168ad295ce6e
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-26 19:12:10 +0900

    up notice.jp

--- notice.js
+++ notice.js
@@ -4,9 +4,12 @@ window.PALMU_NOTICES = [
 	{date: '2025-08-24T14:30', text: 'PalHub 14:00-14:30',
 	 url: 'https://x.com/palmu_jp/status/1957763773932130628'},
 	{date: '2025-08-26T15:20', text: 'メンテ 14:00-15:30 [FB条件3，表示タブ，PayPay]',
-     url: 'https://x.com/palmu_jp/status/1958740914500735062'},
+	 url: 'https://x.com/palmu_jp/status/1958740914500735062'},
 	{date: '2025-08-31', text: 'セール 8/25-8/31',
 	 url: 'https://x.com/palmu_jp/status/1959088203040002408'},
 	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
-     url: 'https://x.com/palmu_jp/status/1948548755935969349'},
+	 url: 'https://x.com/palmu_jp/status/1948548755935969349'},
+	{date: '2025-09-14', text: 'youtube live 14:00-'},
 ];
+
+
