27be3d69ba0301a741b9646c3314ec21bf1d833a
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-08-27 15:05:43 +0900

    セール: team challenge

--- notice.js
+++ notice.js
@@ -5,10 +5,13 @@ window.PALMU_NOTICES = [
 	 url: 'https://x.com/palmu_jp/status/1957763773932130628'},
 	{date: '2025-08-26T15:20', text: 'メンテ 14:00-15:30 [FB条件3，表示タブ，PayPay]',
 	 url: 'https://x.com/palmu_jp/status/1958740914500735062'},
-	{date: '2025-08-31', text: 'セール 8/25-8/31',
+	{date: '2025-08-31', text: 'セール 8/25-8/31 [777]',
 	 url: 'https://x.com/palmu_jp/status/1959088203040002408'},
 	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
 	 url: 'https://x.com/palmu_jp/status/1948548755935969349'},
+	{date: '2025-09-03', text: 'セール 8/27 19:00-9/03 [team]',
+	 start: '2025-08-27',
+	 url: 'https://x.com/palmu_jp/status/1960583057443852404'},
 	{date: '2025-09-14', text: 'youtube live 14:00-'},
 ];
 
