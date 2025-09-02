6de5d2d25d42bda3fcc5f9eda5fefedf6611a8a9
(ru) <222255200+ru-palmu@users.noreply.github.com>
2025-09-02 19:35:04 +0900

    events: 2025-08-26 開始分まで終了

--- notice.js
+++ notice.js
@@ -2,15 +2,19 @@
 /* exported PALMU_NOTICES */
 window.PALMU_NOTICES = [
 	{date: '2025-08-24T14:30', text: 'PalHub 14:00-14:30',
+	 kind: 'PalHub',
 	 url: 'https://x.com/palmu_jp/status/1957763773932130628'},
 	{date: '2025-08-26T15:20', text: 'メンテ 14:00-15:30 [FB条件3，表示タブ，PayPay]',
+	 kind: 'maintenance',
 	 url: 'https://x.com/palmu_jp/status/1958740914500735062'},
 	{date: '2025-08-31', text: 'セール 8/25-8/31 [777, 1.24]',
+	 kind: 'sale',
 	 url: 'https://x.com/palmu_jp/status/1959088203040002408'},
 	{date: '2025-09-01', text: 'ユーザランクが14ランクに',
+	 kind: 'maintenance',
 	 url: 'https://x.com/palmu_jp/status/1948548755935969349'},
 	{date: '2025-09-03', text: 'セール 8/27 19:00-9/03 [team, 1.24]',
-	 start: '2025-08-27',
+	 kind: 'sale', start: '2025-08-27',
 	 url: 'https://x.com/palmu_jp/status/1960583057443852404'},
 	{date: '2025-09-14', text: 'youtube live 14:00-'},
 ];
